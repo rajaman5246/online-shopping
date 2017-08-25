fb405385d677cbc55a44e7639e9d94d5489d1a6a
raj aman <rajaman5246@gmail.com>
2017-08-25 17:45:27 +0530

    My Project Setup completed successfully.

---
+++ shoppingbackend/src/main/java/com/asr/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.asr.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
